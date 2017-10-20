c3eddf406dfa4c3ab64ae63fbe91dd1f4e419d3c
vagrant <kiel.strang@gmail.com>
2017-10-20 21:07:22 +0000

    Jest webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,25 @@ module.exports = {
     publicPath: '/build/'
   },
   module: {
+    loaders: [
+      { exclude: ['node_modules'], loader: 'babel', test: /\.jsx?$/ },
+      { loader: 'style-loader!css-loader', test: /\.css$/ },
+      { loader: 'url-loader', test: /\.gif$/ },
+      { loader: 'file-loader', test: /\.(ttf|eot|svg)$/ },
+    ],
+    resolve: {
+      alias: {
+        config$: './configs/app-config.js',
+        react: './vendor/react-master',
+      },
+      extensions: ['', 'js', 'jsx'],
+      modules: [
+        'node_modules',
+        'bower_components',
+        'shared',
+        '/shared/vendor/modules',
+      ],
+    },
     rules: [
       {
         test: /\.jsx?$/,
