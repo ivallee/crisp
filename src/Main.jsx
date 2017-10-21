eae84d9711e565a8404f65f9ed2bb8e484542677
Isaac Vallee <isaacvallee@gmail.com>
2017-10-21 21:53:51 +0000

    add switch and routes for results

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
 import Search from './Search.jsx';
+import Results from './__Results.jsx';
 
 class Main extends Component {
   render() {
     return (
-      <Search/>
+      
+      <Switch>
+        <Route exact path='/' component={Search}/>
+        <Route path='/results' component={Results}/>
+      </Switch>
+
     );
     //React-Router switch to <SearchResults> or <RecipeDetails>
   }
