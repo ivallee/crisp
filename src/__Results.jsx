49e2d8c037da3fa3c7c8ba4d60a6d007abab4bf7
Isaac Vallee <isaacvallee@gmail.com>
2017-10-21 21:53:12 +0000

    add dummy results component

---
+++ src/__Results.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PropTypes from 'proptypes';
+
+class Results extends Component {
+
+  render() {
+    return (
+      <div className="jumbotron">
+        <p className="lead">This is where results should go!</p>
+      </div>
+    );
+  }
+}
+export default Results;
+
