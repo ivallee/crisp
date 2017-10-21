bed6405b970fd15906c5c0be72e086b90ab3873a
vagrant <kiel.strang@gmail.com>
2017-10-21 23:47:58 +0000

    Rename page components

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from './__Home.jsx'
-import SearchResults from './__SearchResults.jsx';
-import RecipeDetails from './__RecipeDetails.jsx';
+import Home from './Home.jsx';
+import SearchResults from './SearchResults.jsx';
+import RecipeDetails from './RecipeDetails.jsx';
 
 class Main extends Component {
   render() {
     return (
-      
+
       <Switch>
         <Route exact path='/' component={Home}/>
         <Route path='/results' component={SearchResults}/>
