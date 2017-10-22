d28cc2682d6a7ea00c085d00904843ce29d0c926
cjwsstrm <cjwesstrom@gmail.com>
2017-10-22 18:25:03 +0000

    Wrapped filter in a modal and commented out second search bar in SearchResults.jsx

--- src/Filters.jsx
+++ src/Filters.jsx
@@ -20,16 +20,25 @@ class Filters extends Component {
     return (
       <div>
         <h4>Filters</h4>
-        <ul className="list-group">
-          {
-            this.props.filters.map((filter, index) => {
-              return filter && <Filter data={filter} key={index} index={index} update={this.props.updateFilter} remove={this.props.removeFilter}/>;
-            })
-          }
-          <li className="list-group-item active" onClick={this.addFilter}>Add a filter</li>
-        </ul>
+        <button className="btn btn-success" data-toggle="modal" data-target=".bd-example-modal-lg">Your Filters</button>
+        <button type="button" className="btn btn-info">Reset filters</button>
+        <div className="modal fade bd-example-modal-lg" tabIndex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+          <div className="modal-dialog modal-lg">
+            <div className="modal-content">
+              <ul className="list-group">
+                {
+                  this.props.filters.map((filter, index) => {
+                    return filter && <Filter data={filter} key={index} index={index} update={this.props.updateFilter} remove={this.props.removeFilter}/>;
+                  })
+                }
+                <li className="list-group-item active" onClick={this.addFilter}>Add a filter</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 export default Filters;
+
--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -7,47 +7,21 @@ class SearchResults extends Component {
   render() {
     return (
       <div>
-        <div className="jumbotron">
-          <p className="lead">This is where results should go!</p>
-        </div>
-
         <Search />
-
         <div className="jumbotron">
           <h3 className="display-3">Results</h3>
           <p className="lead">Here are some recipes based on your search criteria.</p>
           <p>
           </p>
         </div>
-        <div className="row marketing">
-        <div className="col">
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Start a new search</label>
-            <input type="text" className="form-control" placeholder="What would you like?"></input>
-          </div>
-          <button className="btn btn-success" data-toggle="modal" data-target=".bd-example-modal-lg">Your Filters</button>
-          <button type="button" className="btn btn-info">Reset filters</button>
-          <div className="modal fade bd-example-modal-lg" tabIndex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-            <div className="modal-dialog modal-lg">
-              <div className="modal-content">
-                <ul className="list-group">
-                  <li className="list-group-item active">Add a filter</li>
-                  <li className="list-group-item">
-                    <div className="form-group row">
-                      <label htmlFor="example-text-input" className="col-2 col-form-label">Dinner</label>
-                      <div className="col-10">
-                        <input className="form-control" type="text" value="Artisanal kale" id="example-text-input"></input>
-                      </div>
-                    </div>  
-                  </li>
-                  <li className="list-group-item">Vegan</li>
-                  <li className="list-group-item">Fast</li>
-                </ul>
-              </div>
+        {/* <div className="row marketing">
+          <div className="col">
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Start a new search</label>
+              <input type="text" className="form-control" placeholder="What would you like?"></input>
             </div>
           </div>
-        </div>
-      </div>
+        </div> */}
 
       <div className="row">
       <div className="col-sm-6">
