5de66a0a63bf32fba267995625d0624aefa9b951
cjwsstrm <cjwesstrom@gmail.com>
2017-10-23 23:41:09 +0000

    Added collapsable display of Filters

--- src/Filters.jsx
+++ src/Filters.jsx
@@ -19,26 +19,23 @@ class Filters extends Component {
   render() {
     return (
       <div id="filter-div">
-        <h4>Filters</h4>
-        <button className="btn btn-success" data-toggle="modal" data-target=".bd-example-modal-lg">Your Filters</button>
-        <button type="button" className="btn btn-info">Reset filters</button>
-        <div className="modal fade bd-example-modal-lg" tabIndex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-          <div className="modal-dialog modal-lg">
-            <div className="modal-content">
-              <ul className="list-group">
-                {
-                  this.props.filters.map((filter, index) => {
-                    return filter && <Filter data={filter} key={index} index={index} update={this.props.updateFilter} remove={this.props.removeFilter}/>;
-                  })
-                }
-                <li className="list-group-item active" onClick={this.addFilter}>Add a filter</li>
-              </ul>
-            </div>
-          </div>
+        <a className="btn btn-primary" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+         Filter Menu
+        </a>
+        <div className="collapse" id="collapseExample">
+          <p></p>
+          <p className="list-group-item active" onClick={this.addFilter}>Add a filter</p>
+          <p></p>
+          <ul className="list-group">
+            {
+              this.props.filters.map((filter, index) => {
+                return filter && <Filter data={filter} key={index} index={index} update={this.props.updateFilter} remove={this.props.removeFilter}/>;
+              })
+            }
+          </ul>
         </div>
       </div>
     );
   }
 }
 export default Filters;
-
