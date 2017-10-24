20af74d5f7f860589932ef9bf6785b27138402de
Isaac Vallee <isaacvallee@gmail.com>
2017-10-24 22:56:36 +0000

    dummy data for recipe query

---
+++ src/_dummyRecipeData.js
@@ -0,0 +1,305 @@
+const dummyRecipeData = {
+    "vegetarian": false,
+    "vegan": false,
+    "glutenFree": false,
+    "dairyFree": true,
+    "veryHealthy": false,
+    "cheap": false,
+    "veryPopular": false,
+    "sustainable": false,
+    "weightWatcherSmartPoints": 9,
+    "gaps": "no",
+    "lowFodmap": false,
+    "ketogenic": false,
+    "whole30": false,
+    "servings": 4,
+    "preparationMinutes": 10,
+    "cookingMinutes": 10,
+    "sourceUrl": "http://feedmephoebe.com/2013/11/job-food52s-pan-roasted-cauliflower/",
+    "spoonacularSourceUrl": "https://spoonacular.com/on-the-job-pan-roasted-cauliflower-from-food52-479101",
+    "aggregateLikes": 225,
+    "spoonacularScore": 97,
+    "healthScore": 46,
+    "creditText": "Feed Me Phoebe",
+    "sourceName": "Feed Me Phoebe",
+    "pricePerServing": 199.25,
+    "extendedIngredients": [
+        {
+            "id": 18079,
+            "aisle": "Pasta and Rice",
+            "image": "https://spoonacular.com/cdn/ingredients_100x100/breadcrumbs.jpg",
+            "consistency": "solid",
+            "name": "breadcrumbs",
+            "amount": 0.5,
+            "unit": "cup",
+            "unitShort": "cup",
+            "unitLong": "cups",
+            "originalString": "1/2 cup fresh breadcrumbs (I used gluten-free!)",
+            "metaInformation": [
+                "fresh",
+                "(I used gluten-free!)"
+            ]
+        },
+        {
+            "id": 11135,
+            "aisle": "Produce",
+            "image": "https://spoonacular.com/cdn/ingredients_100x100/cauliflower.jpg",
+            "consistency": "solid",
+            "name": "cauliflower",
+            "amount": 1,
+            "unit": "head",
+            "unitShort": "head",
+            "unitLong": "head",
+            "originalString": "1 head of cauliflower",
+            "metaInformation": []
+        },
+        {
+            "id": 11297,
+            "aisle": "Produce",
+            "image": "https://spoonacular.com/cdn/ingredients_100x100/parsley.jpg",
+            "consistency": "solid",
+            "name": "fresh parsley",
+            "amount": 1,
+            "unit": "handful",
+            "unitShort": "handful",
+            "unitLong": "handful",
+            "originalString": "1 handful parsley, chopped",
+            "metaInformation": [
+                "chopped"
+            ]
+        },
+        {
+            "id": 2063,
+            "aisle": "Produce",
+            "image": "https://spoonacular.com/cdn/ingredients_100x100/rosemary.jpg",
+            "consistency": "solid",
+            "name": "fresh rosemary",
+            "amount": 2,
+            "unit": "teaspoons",
+            "unitShort": "tsp",
+            "unitLong": "teaspoons",
+            "originalString": "2 teaspoons fresh rosemary, chopped",
+            "metaInformation": [
+                "fresh",
+                "chopped"
+            ]
+        },
+        {
+            "id": 9297,
+            "aisle": "Dried Fruits;Produce;Baking",
+            "image": "https://spoonacular.com/cdn/ingredients_100x100/golden-raisins.jpg",
+            "consistency": "solid",
+            "name": "golden raisins",
+            "amount": 0.25,
+            "unit": "cup",
+            "unitShort": "cup",
+            "unitLong": "cups",
+            "originalString": "1/4 cup golden raisins, chopped",
+            "metaInformation": [
+                "chopped"
+            ]
+        },
+        {
+            "id": 4053,
+            "aisle": "Oil, Vinegar, Salad Dressing",
+            "image": "https://spoonacular.com/cdn/ingredients_100x100/olive-oil.jpg",
+            "consistency": "liquid",
+            "name": "olive oil",
+            "amount": 0.25,
+            "unit": "cup",
+            "unitShort": "cup",
+            "unitLong": "cups",
+            "originalString": "1/4 cup olive oil, divided",
+            "metaInformation": [
+                "divided"
+            ]
+        },
+        {
+            "id": 12147,
+            "aisle": "Produce;Baking",
+            "image": "https://spoonacular.com/cdn/ingredients_100x100/pine-nuts.jpg",
+            "consistency": "solid",
+            "name": "pine nuts",
+            "amount": 0.5,
+            "unit": "cup",
+            "unitShort": "cup",
+            "unitLong": "cups",
+            "originalString": "1/2 cup pine nuts",
+            "metaInformation": []
+        },
+        {
+            "id": 1012047,
+            "aisle": "Spices and Seasonings",
+            "image": "https://spoonacular.com/cdn/ingredients_100x100/salt.jpg",
+            "consistency": "solid",
+            "name": "sea salt",
+            "amount": 1,
+            "unit": "teaspoon",
+            "unitShort": "tsp",
+            "unitLong": "teaspoon",
+            "originalString": "1 teaspoon sea salt",
+            "metaInformation": []
+        },
+        {
+            "id": 10211111,
+            "aisle": "Ethnic Foods;Spices and Seasonings",
+            "image": "https://spoonacular.com/cdn/ingredients_100x100/sumac.png",
+            "consistency": "solid",
+            "name": "sumac",
+            "amount": 0.5,
+            "unit": "teaspoon",
+            "unitShort": "tsp",
+            "unitLong": "teaspoons",
+            "originalString": "1/2 teaspoon sumac",
+            "metaInformation": []
+        }
+    ],
+    "id": 479101,
+    "title": "On the Job: Pan Roasted Cauliflower From Food52",
+    "readyInMinutes": 20,
+    "image": "https://spoonacular.com/recipeImages/479101-556x370.jpg",
+    "imageType": "jpg",
+    "cuisines": [],
+    "dishTypes": [
+        "side dish"
+    ],
+    "diets": [
+        "dairy free"
+    ],
+    "occasions": [],
+    "instructions": "Cut the florets off the stems and and then chop them into tiny florets. You can also chop up the stems into tiny pieces if you want. You should have about 6 cups of chopped cauliflower. In a large skillet heat 2 tablespoons of olive oil over medium-high heat. Add the cauliflower, 1 teaspoon of salt, rosemary, and sumac. Sauté until cauliflower is tender and starts to brown a bit, stirring as necessary, about 15 minutes. You can also add a bit of olive oil if the pan starts to get too dry or the cauliflower is starting to stick. Meanwhile, in a small skillet, toast the pinenuts over medium heat until golden brown. Set aside. Heat the remaining 2 tablespoons of olive oil in the same pan. Once oil is shimmering, toss in the breadcrumbs and stir, toasting the breadcrumbs. Season with a pinch of kosher salt and a few turns of freshly ground black pepper. Remove from the heat and toss in half of the chopped parsley. When cauliflower is done, remove from the heat and season to taste with freshly ground black pepper and a pinch or so of salt if necessary. Toss in the toasted pine nuts, the chopped raisins, and the remaining parsley. When ready to serve, sprinkle the top with the toasted breadcrumbs and some pecorino.",
+    "analyzedInstructions": [
+        {
+            "name": "",
+            "steps": [
+                {
+                    "number": 1,
+                    "step": "Cut the florets off the stems and and then chop them into tiny florets. You can also chop up the stems into tiny pieces if you want. You should have about 6 cups of chopped cauliflower. In a large skillet heat 2 tablespoons of olive oil over medium-high heat.",
+                    "ingredients": [
+                        {
+                            "id": 11135,
+                            "name": "cauliflower",
+                            "image": "https://spoonacular.com/cdn/ingredients_100x100/cauliflower.jpg"
+                        },
+                        {
+                            "id": 4053,
+                            "name": "olive oil",
+                            "image": "https://spoonacular.com/cdn/ingredients_100x100/olive-oil.jpg"
+                        }
+                    ],
+                    "equipment": [
+                        {
+                            "id": 404645,
+                            "name": "frying pan",
+                            "image": "https://spoonacular.com/cdn/equipment_100x100/pan.png"
+                        }
+                    ]
+                },
+                {
+                    "number": 2,
+                    "step": "Add the cauliflower, 1 teaspoon of salt, rosemary, and sumac. Sauté until cauliflower is tender and starts to brown a bit, stirring as necessary, about 15 minutes. You can also add a bit of olive oil if the pan starts to get too dry or the cauliflower is starting to stick. Meanwhile, in a small skillet, toast the pinenuts over medium heat until golden brown. Set aside.",
+                    "ingredients": [
+                        {
+                            "id": 11135,
+                            "name": "cauliflower",
+                            "image": "https://spoonacular.com/cdn/ingredients_100x100/cauliflower.jpg"
+                        },
+                        {
+                            "id": 4053,
+                            "name": "olive oil",
+                            "image": "https://spoonacular.com/cdn/ingredients_100x100/olive-oil.jpg"
+                        },
+                        {
+                            "id": 12147,
+                            "name": "pine nuts",
+                            "image": "https://spoonacular.com/cdn/ingredients_100x100/pine-nuts.jpg"
+                        },
+                        {
+                            "id": 10211111,
+                            "name": "sumac",
+                            "image": "https://spoonacular.com/cdn/ingredients_100x100/sumac.png"
+                        },
+                        {
+                            "id": 2047,
+                            "name": "salt",
+                            "image": "https://spoonacular.com/cdn/ingredients_100x100/salt.jpg"
+                        }
+                    ],
+                    "equipment": [
+                        {
+                            "id": 404645,
+                            "name": "frying pan",
+                            "image": "https://spoonacular.com/cdn/equipment_100x100/pan.png"
+                        }
+                    ],
+                    "length": {
+                        "number": 15,
+                        "unit": "minutes"
+                    }
+                },
+                {
+                    "number": 3,
+                    "step": "Heat the remaining 2 tablespoons of olive oil in the same pan. Once oil is shimmering, toss in the breadcrumbs and stir, toasting the breadcrumbs. Season with a pinch of kosher salt and a few turns of freshly ground black pepper.",
+                    "ingredients": [
+                        {
+                            "id": 18079,
+                            "name": "breadcrumbs",
+                            "image": "https://spoonacular.com/cdn/ingredients_100x100/breadcrumbs.jpg"
+                        },
+                        {
+                            "id": 1082047,
+                            "name": "kosher salt",
+                            "image": "https://spoonacular.com/cdn/ingredients_100x100/salt.jpg"
+                        },
+                        {
+                            "id": 4053,
+                            "name": "olive oil",
+                            "image": "https://spoonacular.com/cdn/ingredients_100x100/olive-oil.jpg"
+                        }
+                    ],
+                    "equipment": [
+                        {
+                            "id": 404645,
+                            "name": "frying pan",
+                            "image": "https://spoonacular.com/cdn/equipment_100x100/pan.png"
+                        }
+                    ]
+                },
+                {
+                    "number": 4,
+                    "step": "Remove from the heat and toss in half of the chopped parsley. When cauliflower is done, remove from the heat and season to taste with freshly ground black pepper and a pinch or so of salt if necessary. Toss in the toasted pine nuts, the chopped raisins, and the remaining parsley. When ready to serve, sprinkle the top with the toasted breadcrumbs and some pecorino.",
+                    "ingredients": [
+                        {
+                            "id": 18079,
+                            "name": "breadcrumbs",
+                            "image": "https://spoonacular.com/cdn/ingredients_100x100/breadcrumbs.jpg"
+                        },
+                        {
+                            "id": 11135,
+                            "name": "cauliflower",
+                            "image": "https://spoonacular.com/cdn/ingredients_100x100/cauliflower.jpg"
+                        },
+                        {
+                            "id": 12147,
+                            "name": "pine nuts",
+                            "image": "https://spoonacular.com/cdn/ingredients_100x100/pine-nuts.jpg"
+                        },
+                        {
+                            "id": 11297,
+                            "name": "parsley",
+                            "image": "https://spoonacular.com/cdn/ingredients_100x100/parsley.jpg"
+                        },
+                        {
+                            "id": 2047,
+                            "name": "salt",
+                            "image": "https://spoonacular.com/cdn/ingredients_100x100/salt.jpg"
+                        }
+                    ],
+                    "equipment": []
+                }
+            ]
+        }
+    ]
+}
+
+module.exports = dummyRecipeData;
