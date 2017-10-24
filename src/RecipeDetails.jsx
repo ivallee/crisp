74dfb7cd0fab53c37b1203002b8c212222fc207e
Isaac Vallee <isaacvallee@gmail.com>
2017-10-24 21:20:53 +0000

    inital render of recipe details with dummy data

--- src/RecipeDetails.jsx
+++ src/RecipeDetails.jsx
@@ -3,33 +3,64 @@ import RecipeDetailsLinks from './RecipeDetailsLinks.jsx';
 import RecipeDetailsIngredients from './RecipeDetailsIngredients.jsx';
 import RecipeDetailsInstructions from './RecipeDetailsInstructions.jsx';
 
+// delete when api calls functioning
+import dummyResults from './dummyresults.js';
+
 class RecipeDetails extends Component {
 
-  render() {
-    return (
-      <div>
-        {/* <div className="jumbotron">
-          <p className="lead">This is where recipe details should go! props.match.params.id is: {parseInt(this.props.match.params.id)} </p>
-        </div> */}
-        <div className="row">
-          <div className="col-sm-6">
-            <img className="img-thumbnail" src={'../docs/grilledcheese.jpg'} alt="Card image cap"></img>
-         </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-6">
-          <h4>
-            Toasty Toast
-            <small className="text-muted"> Time required: 30 minutes</small>
-          </h4>    
-          </div>
-        </div>
-        < RecipeDetailsLinks />
-        < RecipeDetailsIngredients />
-        < RecipeDetailsInstructions />
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = { 
+      response: [] 
+    };
   }
+
+  componentWillMount() {
+
+  // Replace this with fetch to API:
+  let response = dummyResults.results.filter((recipe) => {
+    return recipe.id == this.props.match.params.id;
+  });
+
+  this.setState({ response });
+
+
+}
+
+
+render() {
+  /////////////////////////
+
+  // const recipeById = this.props.searchResponse.filter((recipe) => {
+  //   return recipe.id == 658674;
+
+  // });
+
+  const recipe = this.state.response[0];
+  console.log(this.state);
+
+  return (
+    <div>
+      <div className="row">
+        <div className="col-sm-6">
+          <img className="img-thumbnail" src={recipe.image} alt="Card image cap"></img>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6">
+          <h4>
+            {recipe.title}
+          </h4>
+          <h5>By {recipe.sourceName}</h5>
+            <small className="text-muted"> Time required: {recipe.readyInMinutes} minutes</small>
+        </div>
+      </div>
+      < RecipeDetailsLinks />
+      < RecipeDetailsIngredients />
+      < RecipeDetailsInstructions />
+    </div>
+  );
+}
 }
 export default RecipeDetails;
 
