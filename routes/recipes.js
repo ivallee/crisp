c46e1ae6bae4608d280e5ddcc43c5d4dfa4f9278
Isaac Vallee <isaacvallee@gmail.com>
2017-10-25 23:36:04 +0000

    builds search api request

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,19 +2,31 @@ require('dotenv').config();
 const express = require('express');
 const rp = require('request-promise-native');
 const errors = require('../lib/custom-errors.js');
-const base64url = require('base64url');
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('placeholder');
 });
 
-router.get('/search/:query', (req, res) => {
-  res.send(`Searching for ${req.params.query}`);
+router.get('/search/:query', (req, res, next) => {
+  // builds api request
+  let request = `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?query=${req.params.query}`;
+  request += '&addRecipeInformation=true&limitLicense=true&instructionsRequired=true&number=8';
+  console.log('query param: ',req.params.query);
+  const options = {
+    uri: request,
+    headers: {'X-Mashape-Key': process.env.MASHAPE_KEY }
+  };
+  rp(options)
+    .then(data => res.send(data))
+    .catch(error => {
+      if(error.statusCode === 404) next(new errors.RecipeNotFound(req.params.id));
+      else next(error);
+    });
 });
 
 router.get('/:id', (req, res, next) => {
-  console.log(req.params.id);
+  console.log('req.params.id: ', req.params.id);
   const options = {
     uri: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${req.params.id}/information`,
     headers: {'X-Mashape-Key': process.env.MASHAPE_KEY }
@@ -26,7 +38,5 @@ router.get('/:id', (req, res, next) => {
       else next(error);
     });
 });
-
-
 
 module.exports = router;
