730bf63d071dc4946ef257dd00fd2130078d2d24
vagrant <kiel.strang@gmail.com>
2017-10-25 21:04:00 +0000

    Separate express and webpack server files

--- ex-server.js
+++ ex-server.js
@@ -1,12 +1,7 @@
 require('dotenv').config();
-const WEBPACK_PORT = process.env.WEBPACK_PORT || 3000;
 const EXPRESS_PORT = process.env.EXPRESS_PORT || 8080;
 const HOST = process.env.HOST || '0.0.0.0';
 const ENV = process.env.ENV || 'development';
-
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-const webpackConfig = require('./webpack.config');
 
 const express = require('express');
 
@@ -18,23 +13,6 @@ const filtersRoutes = require('./routes/filters');
 const usersRoutes = require('./routes/users');
 const recipesRoutes = require('./routes/recipes');
 const errorHandler = require('./routes/error-handler.js');
-
-new WebpackDevServer(webpack(webpackConfig), {
-  publicPath: webpackConfig.output.publicPath,
-  historyApiFallback: true,
-  watchOptions: {
-    aggregateTimeout: 300,
-    poll: 1000,
-    ignored: /node_modules/
-  }
-})
-  .listen(WEBPACK_PORT, HOST, (err) => {
-    if(err) {
-      console.log(err);
-    }
-    console.log(`Webpack running at http://${HOST}:${WEBPACK_PORT}`);
-  });
-
 
 const app = express();
 
