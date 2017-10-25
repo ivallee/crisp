ddd087d7dd0b84f07e41fe559566a887421983e3
vagrant <kiel.strang@gmail.com>
2017-10-25 01:07:34 +0000

    Merge options into getFilter, add getFilterTypes

--- routes/filters.js
+++ routes/filters.js
@@ -3,18 +3,13 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get('/', (req, res) => {
-    db.getAllFilters()
+    db.getFilterTypes()
       .then(filters => res.send(JSON.stringify(filters)));
   });
 
   router.get('/:id', (req, res) => {
     db.getFilter(req.params.id)
       .then(filter => res.send(JSON.stringify(filter)));
-  });
-
-  router.get('/:id/options', (req, res) => {
-    db.getFilterOptions(req.params.id)
-      .then(options => res.send(JSON.stringify(options)));
   });
 
   return router;
