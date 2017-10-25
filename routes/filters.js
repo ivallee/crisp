b53218588150acd5bdaaa477572d2bb412c1bd18
vagrant <kiel.strang@gmail.com>
2017-10-25 00:40:11 +0000

    Add single filter and options routes

--- routes/filters.js
+++ routes/filters.js
@@ -3,8 +3,18 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get('/', (req, res) => {
-    db.getFilters()
+    db.getAllFilters()
       .then(filters => res.send(JSON.stringify(filters)));
+  });
+
+  router.get('/:id', (req, res) => {
+    db.getFilter(req.params.id)
+      .then(filter => res.send(JSON.stringify(filter)));
+  });
+
+  router.get('/:id/options', (req, res) => {
+    db.getFilterOptions(req.params.id)
+      .then(options => res.send(JSON.stringify(options)));
   });
 
   return router;
