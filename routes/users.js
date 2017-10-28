3fd269e204e6a31481cff06abb7bbbfe27ca2ef1
vagrant <kiel.strang@gmail.com>
2017-10-28 19:26:16 +0000

    crud routes for categories

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ module.exports = (db) => {
       req.session.user_id = id;
       res.send(`Created user ${id}`);
     }
-    catch (err) {
+    catch(err) {
       next(err);
     }
   });
@@ -22,7 +22,7 @@ module.exports = (db) => {
       req.session.user_id = user.id;
       res.send({ message: 'Login successful', success: true });
     }
-    catch (err) {
+    catch(err) {
       next(err);
     }
   });
@@ -35,6 +35,44 @@ module.exports = (db) => {
   router.get('/current', check.isAuthenticated, (req, res) => {
     res.send(res.locals.user);
   });
+
+  router.route('/categories')
+    .post(check.isAuthenticated, async (req, res, next) => {
+      try {
+        const category_id = await db.createCategory(req.session.user_id, req.body.name);
+        res.status(201).send({ message: `Category ${category_id} created`, category_id });
+      } catch(err) {
+        next(err);
+      }
+    })
+    .get(check.isAuthenticated, async (req, res, next) => {
+      try {
+        const categories = await db.getUserCategories(req.session.user_id);
+        res.send(categories);
+      } catch(err) {
+        next(err);
+      }
+    })
+    .put(check.isAuthenticated, async (req, res, next) => {
+      try {
+        const {id, name} = req.body;
+        await db.renameCategory(id, req.session.user_id, name);
+        res.send(`Category ${id} renamed to ${name}`);
+      } catch(err) {
+        next(err);
+      }
+    })
+    .delete(check.isAuthenticated, async (req, res, next) => {
+      try {
+        console.log('ID: ', req.body.id);
+        await db.deleteCategory(req.body.id, req.session.user_id);
+        res.send({ message:  `Category ${req.body.id} deleted`});
+      } catch(err) {
+        next(err);
+      }
+    });
+
+
 
   return router;
 };
