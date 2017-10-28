ce1f50bf7def0b8dcebd0d6fd213a9f60726b1d1
vagrant <kiel.strang@gmail.com>
2017-10-28 21:18:37 +0000

    Clean up error handling

--- routes/error-handler.js
+++ routes/error-handler.js
@@ -1,17 +1,10 @@
 const errors = require('../lib/custom-errors.js');
 
+const errorTypes = Object.keys(errors).map(key => errors[key]);
+
 module.exports = (err, req, res, next) => {
   console.error('Error:', err.message);
-  if(err instanceof errors.RecipeNotFound) {
-    return res.status(err.status).send(err.message);
-  }
-  if(err instanceof errors.InvalidLogin) {
-    return res.status(err.status).send(err.message);
-  }
-  if(err instanceof errors.NotLoggedIn) {
-    return res.status(err.status).send(err.message);
-  }
-  if(err instanceof errors.MissingParam) {
+  if(errorTypes.some(type => err instanceof type)) {
     return res.status(err.status).send(err.message);
   }
   return res.status(500).send('An internal error occured');
