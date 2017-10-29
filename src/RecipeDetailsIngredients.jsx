8f65a47b633a3dec93d8af73b46ced0dcb94dd94
Isaac Vallee <isaacvallee@gmail.com>
2017-10-29 01:48:25 +0000

    make ingred list small

--- src/RecipeDetailsIngredients.jsx
+++ src/RecipeDetailsIngredients.jsx
@@ -9,7 +9,7 @@ function RecipeDetailsIngredients({ ingredient }) {
 
   
   return (
-    <li>{ingredient}</li>
+    <li><small>{ingredient}</small></li>
   );
 }
 export default RecipeDetailsIngredients;
