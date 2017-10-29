f91049653d3dae124d882faade2428c01a956a56
Isaac Vallee <isaacvallee@gmail.com>
2017-10-29 01:38:45 +0000

    take out links component

--- src/RecipeDetails.jsx
+++ src/RecipeDetails.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import RecipeDetailsLinks from './RecipeDetailsLinks.jsx';
+import { Link } from 'react-router-dom';
 import RecipeDetailsIngredients from './RecipeDetailsIngredients.jsx';
 import RecipeDetailsInstructions from './RecipeDetailsInstructions.jsx';
 import axios from 'axios';
@@ -58,7 +58,7 @@ class RecipeDetails extends Component {
     return (
       <div className='jumbotron content-blocks'>
         <div className="row">
-          <div className="col-sm-6 recipe-details-img-container">
+          <div className="col-sm-6 d-flex justify-content-center">
             <img className='recipe-details-img' src={recipe.image} alt="Recipe image"></img>
           </div>
           <div className="col-sm-6">
@@ -67,9 +67,13 @@ class RecipeDetails extends Component {
             </h4>
             <h5>By {recipe.sourceName}</h5>
             <small className="text-muted"> Time required: {recipe.readyInMinutes} minutes</small>
-            < RecipeDetailsLinks />
+            <div className="recipe-details-links d-flex justify-content-between">
+              <button type="button" className="btn btn-info">Save this recipe</button>
+              {/* Make buttons functional */}
+              <Link to='/results'><button type="button" className="btn btn-info">Back to reccomendations</button></Link>
+            </div>
           </div>
-          
+
         </div>
         <div className="row recipe-info">
           <div className='col-6'>
