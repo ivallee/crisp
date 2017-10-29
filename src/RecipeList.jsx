db8b69f5b5fb78e7e59c66a72e930e8966843adc
vagrant <kiel.strang@gmail.com>
2017-10-29 23:03:01 +0000

    Implement SearchRecipes container

--- src/RecipeList.jsx
+++ src/RecipeList.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'proptypes';
 import RecipeCard from './RecipeCard.jsx';
-import { getUserRecipes } from './api.js';
 
 class RecipeList extends Component {
 
@@ -13,12 +12,19 @@ class RecipeList extends Component {
   }
 
   static propTypes = {
-    recipes: PropTypes.array
+    recipes: PropTypes.array,
+    recipeCount: PropTypes.number
   }
 
   removeRecipe = (index) => {
-    this.state.recipes.splice(index, 1, this.state.recipes[4]);
-    this.state.recipes.splice(4, 1);
+    if(this.props.recipeCount) {
+      //replace with the first unused recipe
+      this.state.recipes.splice(index, 1, this.state.recipes[this.props.recipeCount]);
+      //remove that recipe from the list
+      this.state.recipes.splice(this.props.recipeCount, 1);
+    } else {
+      this.state.recipes.splice(index, 1);
+    }
     this.setState({
       recipes: this.state.recipes
     });
@@ -26,7 +32,8 @@ class RecipeList extends Component {
   }
 
   renderRecipes = () => {
-    return this.state.recipes.slice(0, 4).map((recipe, index) => {
+    const recipes = this.props.recipeCount? this.state.recipes.slice(0, this.props.recipeCount) : this.state.recipes;
+    return recipes.map((recipe, index) => {
       return <RecipeCard
         id={recipe.id}
         index={index}
@@ -34,6 +41,7 @@ class RecipeList extends Component {
         image={recipe.image}
         time={recipe.readyInMinutes}
         servings={recipe.servings}
+        saved={recipe.saved}
         sourceName={recipe.sourceName}
         key={recipe.id}
         removeRecipe={this.removeRecipe}
@@ -42,17 +50,12 @@ class RecipeList extends Component {
   };
 
   render() {
-    const recipeCards = this.renderRecipes();
-    getUserRecipes().then(response => console.log(response));
     return (
-
       <div className="recipe-list">
         <div className="row">
-          {recipeCards}
+          {this.renderRecipes()}
         </div>
       </div>
-
-
     );
   }
 }
--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'proptypes';
-import Search from './Search.jsx';
-import RecipeList from './RecipeList.jsx';
+import SearchRecipes from './SearchRecipes.jsx';
 
 class SearchResults extends Component {
 
@@ -11,20 +9,15 @@ class SearchResults extends Component {
   }
 
   render() {
-    const recipes = this.props.searchResponse;
     return (
       <div className="results">
-
-        
         <div id="search-result-list" className='jumbotron content-blocks'>
-        <h3 id="search-result-list-header" className="text-center">Reccomendations:</h3>
-        <RecipeList recipes={recipes} />
-        <div className='d-flex justify-content-center'>
-        <a className='back-btn' href='/'><button className="btn btn-success">Do a different search</button></a>
+          <h3 id="search-result-list-header" className="text-center">Recommendations:</h3>
+          <SearchRecipes {...this.props} />
+          <div className='d-flex justify-content-center'>
+            <a className='back-btn' href='/'><button className="btn btn-success">Do a different search</button></a>
+          </div>
         </div>
-        </div>
-
-
       </div>
     );
   }
