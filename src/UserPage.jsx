da1654a892d61f63bfbe15bcd2a7360ac9f42edf
cjwsstrm <cjwesstrom@gmail.com>
2017-10-30 18:51:24 +0000

    Updated userpage to integrate with filters and recipes

--- src/UserPage.jsx
+++ src/UserPage.jsx
@@ -32,34 +32,79 @@ class UserPage extends Component {
         return recipe;
       });
     return (
-      <div>
-        <RecipeContainer recipes={recipes} savedRecipes={this.props.savedRecipes} userUpdated={this.props.userUpdated} />
-        <Filters filters={this.props.savedFilters} addFilter={this.addFilter} removeFilter={this.removeFilter} updateFilter={this.updateFilter} />
+      <div className="col user-recipes">
+        <div className="user-page-jumbo">
+          <h5 className="display-4">Hello Grateful User</h5>
+          <p className="">Crisp is a recipe recommendation engine for people who are looking to try cooking more meatless and plant-based meals.</p>
+          <p className="">On this page you can update your preset filters and manage your recipes. The filters will be set automatically for you when you do a search.</p>
+          <p className="">You can disable them at the search page as well, if you would like to exclude them temporarily.</p>
+          <p className="">Management of your recipes is done through categorization. You can create new categories, remove old ones, and move your saved recipes to another category </p>
+        </div>
+        <div className="row">
+          <div className="col-4">
+            <a href="#demo" className="btn btn-secondary col" data-toggle="collapse">Your preset filters</a>
+            <div id="demo" className="collapse col">
+              <ul className="list-group col-10">
+                <Filters filters={this.props.savedFilters} addFilter={this.addFilter} removeFilter={this.removeFilter} updateFilter={this.updateFilter} />
+              </ul>
+            </div>
+            <a href="#demo2" className="btn btn-secondary col" data-toggle="collapse">Your Recipes</a>
+            <div id="demo2" className="collapse col">
+              <ul className="list-group col-10">
+                <li className="list-group-item"> <a href="#demo3" className="btn btn-secondary col" data-toggle="collapse">Dinner</a></li>
+                <li className="list-group-item">Breakfast</li>
+                <li className="list-group-item">Spicy</li>
+                <li className="list-group-item">Other people's favorites</li>
+                <li className="list-group-item">Sunday Meals</li>
+              </ul>
+            </div>
+          </div>
+          <div className="user-recipe-list-card">
+          </div>
+          <div id="demo3" className="collapse col">
+            <RecipeContainer recipes={recipes} savedRecipes={this.props.savedRecipes} userUpdated={this.props.userUpdated} />
+            <div className="user-recipe-list-card">
+              <div className="card">
+                <div className="card-block">
+                  {/* <Link to={`/recipes/${this.props.id}`} className="recipe-card-link"> */}
+                  <div className="row">
+                    <div className="col">
+                      <h4 className="card-title">Yummy</h4>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-sm-6">
+                      <img className="img-thumbnail" src={'/docs/grilledcheese.jpg'} alt="recipe thumbnail"></img>
+                    </div>
+                    <div className="col-sm-6">
+                      <ul className="list-unstyled">
+                        <li>
+                          <small>Time: Short</small>
+                        </li>
+                        <li>
+                          <small>From: Home</small>
+                        </li>
+                        <li>
+                          <small>Servings: plenty</small>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  {/* </ Link> */}
+                  <div className="btn-group btn-group-sm" role="group">
+                    <button type="button" className="close" aria-label="Remove" onClick={e => this.remove(e)}>
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                    <button type="button" className="btn btn-danger">Never</button>
+                    <button type="button" className="btn btn-success">Try later</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      // <div className="col user-recipes">
-      //   <div className="row">
-      //     <div className="col-4">
-      //   <a href="#demo" className="btn btn-secondary col" data-toggle="collapse">Your preset filters</a>
-      //    <div id="demo" className="collapse col">
-      //     <ul className="list-group">
-      //       <li className="list-group-item">Cras justo odio</li>
-      //       <li className="list-group-item">Dapibus ac facilisis in</li>
-      //     </ul>
-      //   </div>
-      //   <a href="#demo2" className="btn btn-secondary col" data-toggle="collapse">Your Recipes</a>
-      //     <div id="demo2" className="collapse col">
-      //       <ul className="list-group">
-      //         <li className="list-group-item"> <a href="#demo3" className="btn btn-secondary col" data-toggle="collapse">Dinner</a></li>
-      //         <li className="list-group-item">Breakfast</li>
-      //       </ul>
-      //     </div>
-      //     </div>
-      //     <div className="user-recipe-list-card">
-      //   </div>
-      //   <div id="demo3" className="collapse col">
-      //   </div>
-      //   </div>
-      // </div>
+
 
     );
   }
