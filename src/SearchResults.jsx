efe26c3aa6484d5104e2197cb51ccba649f2478d
vagrant <kiel.strang@gmail.com>
2017-11-01 17:31:53 +0000

    Search results link back uses react-router

--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import PropTypes from 'proptypes';
 import RecipeContainer from './RecipeContainer.jsx';
 
@@ -15,9 +16,9 @@ class SearchResults extends Component {
       <div className="results">
         <div id="search-result-list" className='jumbotron content-blocks'>
           <h3 id="search-result-list-header" className="text-center">Recommendations:</h3>
-          <RecipeContainer recipes = {this.props.searchResponse} savedRecipes = {this.props.savedRecipes} userUpdated = {this.props.userUpdated} count={4}/>
+          <RecipeContainer recipes={this.props.searchResponse} savedRecipes={this.props.savedRecipes} userUpdated={this.props.userUpdated} count={4} />
           <div className='d-flex justify-content-center'>
-            <a className='back-btn' href='/'><button className="btn btn-success">Do a different search</button></a>
+            <NavLink className="nav-link" activeClassName="nav-link" to='/'><button className="btn btn-success">Do a different search</button></NavLink>
           </div>
         </div>
       </div>
